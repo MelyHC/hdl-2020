7ae30a0faf960217aa0709509cff099b6f6b95a1
MelyHC <mely.hidalgo.crespo@gmail.com>
2020-08-22 14:08:11 -0500

    Updates

---
+++ precache-manifest.4ca8c93030b3a748c0f1dde454416e80.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "625fa276a15aea00ac8b9369d3ce135a",
+    "url": "/hdl-2020/index.html"
+  },
+  {
+    "revision": "1f5b19b9493695a075a0",
+    "url": "/hdl-2020/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "04505a4120595d80b911",
+    "url": "/hdl-2020/static/css/main.128c77e3.chunk.css"
+  },
+  {
+    "revision": "1f5b19b9493695a075a0",
+    "url": "/hdl-2020/static/js/2.34c2e449.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/hdl-2020/static/js/2.34c2e449.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04505a4120595d80b911",
+    "url": "/hdl-2020/static/js/main.add9561c.chunk.js"
+  },
+  {
+    "revision": "a6174cdc64ba296644c5",
+    "url": "/hdl-2020/static/js/runtime-main.27593cfa.js"
+  },
+  {
+    "revision": "6f13edb545c55dec6003e7be3aebd872",
+    "url": "/hdl-2020/static/media/icon-user.6f13edb5.png"
+  },
+  {
+    "revision": "c4d51d5ff750e67c37fb57def5e2cbac",
+    "url": "/hdl-2020/static/media/img-welcome.c4d51d5f.png"
+  },
+  {
+    "revision": "f4eb7cff4ffcd9b333e5c997ea9d433c",
+    "url": "/hdl-2020/static/media/logo-principal.f4eb7cff.png"
+  }
+]);
