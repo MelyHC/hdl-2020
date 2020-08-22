dc5f1b0ccd9355baffe86da3cd49fb1d825d6a9c
sumaqkuyay <sabina.canchumani.huaman@gmail.com>
2020-08-22 14:04:57 -0500

    cambios

--- src/view/Home.jsx
+++ src/view/Home.jsx
@@ -5,15 +5,27 @@ import CreatePost from '../components/CreatePost';
 import PostCard from '../components/PostCard';
 import UserInfo from '../components/UserInfo';
 import InfoCard from '../components/InfoCard';
+import { getCurrentUser } from '../controller/firebase';
+import {getUser} from '../controller/firestore';
 
 const Home = () => {
+  const currentUser = getCurrentUser();
+  // console.log(getCurrentUser().name);
+  getUser(currentUser.email).then((data)=>{
+    data.forEach((doc)=>{
+      // console.log(doc);
+    });
+    // console.log(data);
+  });
+  // console.log(currentUserData);
+  // console.log(currentUser);
   return (
     <div className="container-fluid bg-gray">
       <NavBar />
       <div className="container pt-5 mt-5">
         <div className="row">
           <div className="col">
-            <UserInfo />
+            <UserInfo currentUser={currentUser} />
           </div>
           <div className="col">
             <CreatePost />
